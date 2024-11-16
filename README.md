1770d0660943a2503d551f94370e62b4c294ff74
theluqmn <luqman.yazrie@outlook.com>
2024-11-17 03:43:08 +0800

    some changes to README, and compiler

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ A temperature converter program made with 64-bit x86 assembly, using NASM.
 
 ## Usage
 
-1. Compile all assembly (.asm) files.
-2. Link all object files (.o) into a single executable.
-3. Run the executable.
+1. Run `bash compile.sh` to compile the code.
+2. Run `./temperate` to run the program.
--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ echo "Compiler tool started for Temperate"
 nasm -f elf64 -o main.o main.asm
 nasm -f elf64 -o console.o scripts/console.asm
 nasm -f elf64 -o converters.o scripts/converters.asm
-ld -o main main.o console.o converters.o
+ld -o temperate main.o console.o converters.o
 
-echo "Compiler tool finished. Run ./main"
+echo "Compiler tool finished. Run ./temperate to run."
